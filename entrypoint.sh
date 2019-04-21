b72060bbefae1a25060adaea880f1d2ec362402b
ghostry <ghostry@ghostry.cn>
2019-04-21 16:40:55 +0000

    增加参数

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,19 @@ if [ ! -f /var/lib/mldonkey/downloads.ini ]; then
     echo "Waiting for mldonkey to start..."
     sleep 5
     /usr/lib/mldonkey/mldonkey_command -p "" "set allowed_ips 0.0.0.0/0" "set temp_directory /temp"
+    #启用网络
     /usr/lib/mldonkey/mldonkey_command -p '' "set enable_kademlia true" "set enable_overnet true"
+    #上传下载速度不限制
+    /usr/lib/mldonkey/mldonkey_command -p '' "set max_hard_download_rate 0" "set max_hard_upload_rate 0"
+    #修改服务器来源
     /usr/lib/mldonkey/mldonkey_command -p '' "set ED2K-update_server_list_client true" "set ED2K-update_server_list_server true" "set ED2K-update_server_list_server_met true"
+    #增加下载共享目录
     /usr/lib/mldonkey/mldonkey_command -p '' "share 0 /incoming/files incoming_files" "share 0 /incoming/directories incoming_directories"
+    #删除自带下载共享目录
     /usr/lib/mldonkey/mldonkey_command -p '' "unshare incoming/files" "unshare incoming/directories"
+    #使共享生效
     /usr/lib/mldonkey/mldonkey_command -p '' "reshare"
+    #保存配置到配置文件
     /usr/lib/mldonkey/mldonkey_command -p '' "save"
     if [ -z "$MLDONKEY_ADMIN_PASSWORD" ]; then
         /usr/lib/mldonkey/mldonkey_command -p "" "kill"
